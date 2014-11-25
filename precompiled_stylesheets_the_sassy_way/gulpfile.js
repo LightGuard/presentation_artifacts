31c3307f934e8bde4e080937f3901c81691484bc
LightGuard <lightguard.jp@gmail.com>
2014-11-25 13:46:37 -0700

    Precompiled Stylesheets talk added

---
+++ precompiled_stylesheets_the_sassy_way/gulpfile.js
@@ -0,0 +1,101 @@
+var pkg = require('./package.json'),
+  gulp = require('gulp'),
+  gutil = require('gulp-util'),
+  plumber = require('gulp-plumber'),
+  rimraf = require('gulp-rimraf'),
+  rename = require('gulp-rename'),
+  connect = require('gulp-connect'),
+  browserify = require('gulp-browserify'),
+  uglify = require('gulp-uglify'),
+  jade = require('gulp-jade'),
+  sass = require('gulp-ruby-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  through = require('through'),
+  opn = require('opn'),
+  path = require('path'),
+  isDist = process.argv.indexOf('serve') === -1,
+  config = {
+    bowerDir: './bower_components'
+  };
+
+gulp.task('js', ['clean:js'], function() {
+  return gulp.src('src/scripts/main.js')
+    .pipe(isDist ? through() : plumber())
+    .pipe(browserify({ transform: ['debowerify'], debug: !isDist }))
+    .pipe(isDist ? uglify() : through())
+    .pipe(rename('build.js'))
+    .pipe(gulp.dest('dist/build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', ['clean:html'], function() {
+  return gulp.src('src/index.jade')
+    .pipe(isDist ? through() : plumber())
+    .pipe(jade({ pretty: true }))
+    .pipe(rename('index.html'))
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload());
+});
+
+gulp.task('css', ['clean:css'], function() {
+  return gulp.src('src/styles/main.scss')
+    .pipe(isDist ? through() : plumber())
+    .pipe(sass({style: (isDist ? 'compress': 'expanded'), precision: '9', loadPath: ['./src/styles']}))
+    .pipe(autoprefixer('last 2 versions', { map: false }))
+    .pipe(gulp.dest('dist/build'))
+    .pipe(connect.reload());
+});
+
+gulp.task('images', ['clean:images'], function() {
+  return gulp.src('src/images/**/*')
+    .pipe(gulp.dest('dist/images'))
+    .pipe(connect.reload());
+});
+
+gulp.task('clean', function() {
+  return gulp.src('dist')
+    .pipe(rimraf());
+});
+
+gulp.task('clean:html', function() {
+  return gulp.src('dist/index.html')
+    .pipe(rimraf());
+});
+
+gulp.task('clean:js', function() {
+  return gulp.src('dist/build/build.js')
+    .pipe(rimraf());
+});
+
+gulp.task('clean:css', function() {
+  return gulp.src('dist/build/build.css')
+    .pipe(rimraf());
+});
+
+gulp.task('clean:images', function() {
+  return gulp.src('dist/images')
+    .pipe(rimraf());
+});
+
+gulp.task('connect', ['build'], function(done) {
+  connect.server({
+    root: 'dist',
+    livereload: true
+  });
+
+  opn('http://localhost:8080', done);
+});
+
+gulp.task('watch', function() {
+  gulp.watch('src/**/*.jade', ['html']);
+  gulp.watch('src/styles/**/*.scss', ['css']);
+  gulp.watch('src/images/**/*', ['images']);
+  gulp.watch([
+    'src/scripts/**/*.js',
+    'bespoke-theme-*/dist/*.js' // Allow themes to be developed in parallel
+  ], ['js']);
+});
+
+gulp.task('build', ['js', 'html', 'css', 'images']);
+gulp.task('serve', ['connect', 'watch']);
+gulp.task('default', ['build']);
---
+++ precompiled_stylesheets_the_sassy_way/src/scripts/main.js
@@ -0,0 +1,27 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  bullets = require('bespoke-bullets'),
+  scale = require('bespoke-scale'),
+  fx = require('bespoke-fx'),
+  classes = require('bespoke-classes'),
+  hash = require('bespoke-hash');
+
+// Bespoke.js
+bespoke.from('article', [
+  classes(),
+  bullets('li, .bullet'),
+  keys(),
+  touch(),
+  scale(),
+  hash(),
+  fx({transition: 'move'})
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+require('prism/components/prism-scss');
+
